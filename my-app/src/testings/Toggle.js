85b037717f01abf447bde04ad9d07fae935fd707
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2021-11-20 17:03:09 +0400

    upd

--- my-app/src/testings/Toggle.js
+++ my-app/src/testings/Toggle.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: true};
+    this.state = { isToggleOn: true };
 
     // Эта привязка обязательна для работы `this` в колбэке.
     // При обращении к this в JSX-колбэках необходимо учитывать, что методы класса в JavaScript по умолчанию не привязаны к контексту.
@@ -16,13 +16,13 @@ class Toggle extends React.Component {
   // Если вы пользуетесь экспериментальным синтаксисом общедоступных полей классов, 
   // такой синтаксис доступен в Create React App по умолчанию, вы можете использовать его, чтобы правильно привязать колбэки:
   // Тогда привязыание контекста this через .bind(this), выше, не потребуется.
-  handleClick = () => {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }  
+  // handleClick = () => {
+  //   this.setState(prevState => ({
+  //     isToggleOn: !prevState.isToggleOn
+  //   }));
+  // }  
 
-  handleClick() {
+  handleClick(id) {
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
@@ -30,6 +30,12 @@ class Toggle extends React.Component {
 
   render() {
     return (
+      // Ещё вариант если метод .bind(this) не нравится можно вызвать событие синтаксисом стрелочной функции.
+      // Проблема этого синтаксиса в том, что при каждом рендере компонента создаётся новый колбэк. 
+      // Чаще всего это не страшно. Однако, если этот колбэк попадает как проп в дочерние компоненты, 
+      // эти компоненты могут быть отрендерены снова. Мы рекомендуем делать привязку в конструкторе 
+      // или использовать синтаксис полей классов, чтобы избежать проблем с производительностью.
+      // <button onClick={(e) => this.handleClick(e)}>
       <button onClick={this.handleClick}>
         {this.state.isToggleOn ? 'Включено' : 'Выключено'}
       </button>
